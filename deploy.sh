43c100f04cba1b1c7f8b5d7d8214190863e6c368
Siva Chunduru <c.sivaprasad@gmail.com>
2022-08-01 03:19:07 +0530

    deploy applications using minikube

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-# this is your part to fill
+# Start minikube
+minikube start
+
+# invoice-app
+# Build image
+cd invoice-app \
+    && docker build -t invoice-app -f Dockerfile .
+
+# From host, push the Docker image directly to minikube
+minikube image load invoice-app:latest
+
+# Deploy
+kubectl apply -f deployment.yaml
+
+# payment-provider
+# Build image
+cd ../payment-provider \
+    && docker build -t payment-provider -f Dockerfile .
+
+# From host, push the Docker image directly to minikube
+minikube image load payment-provider:latest
+
+# Deploy deployment
+kubectl apply -f deployment.yaml
+
+# Check that it's running
+kubectl get pods
